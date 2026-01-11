4a8e99c579cf47679404c9ecb7fc751b33e7d9dd
SMDTANZIMS173 <smdtanzimsultana@gmail.com>
2026-01-11 12:46:33 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/home.component')
+        .then(m => m.HomeComponent)
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./features/about/about.component')
+        .then(m => m.AboutComponent)
+  },
+  {
+    path: 'projects',
+    loadComponent: () =>
+      import('./features/projects/projects.component')
+        .then(m => m.ProjectsComponent)
+  },
+  {
+    path: 'skills',
+    loadComponent: () =>
+      import('./features/skills/skills.component')
+        .then(m => m.SkillsComponent)
+  },
+  {
+    path: 'creative',
+    loadComponent: () =>
+      import('./features/creative/creative.component')
+        .then(m => m.CreativeComponent)
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./features/contact/contact.component')
+        .then(m => m.ContactComponent)
+  }
+];
