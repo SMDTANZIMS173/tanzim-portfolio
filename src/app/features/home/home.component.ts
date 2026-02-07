0d76c3a78efcf67e79601c3c3e7c752362d18448
SMDTANZIMS173 <smdtanzimsultana@gmail.com>
2026-02-07 19:26:14 +0530

    Corrected Page Load Event

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.trackClick();
+  }
+
+  trackClick(): void {
+    fetch(environment.webhookUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        secret: environment.secretKey,
+        source: "home-visit",
+        userAgent: navigator.userAgent,
+        note: "portfolio home visit"
+      })
+    }).catch(err => console.error("Tracking failed", err));
+  }
 
 }
